463bd05dc333e78ac9707beb56e93af561d9bedf
rosicka_i <ilzerosicka@startschool.org>
2024-11-15 10:53:46 +0200

    Second part added

--- my_first_scraper.py
+++ my_first_scraper.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 def request_github_trending(url):
 
@@ -12,7 +13,33 @@ def request_github_trending(url):
         return None
 
 #Testing
+"""
 html_content = request_github_trending("https://github.com/trending")
 if html_content:
     print("Got content successfully!")
-    print(html_content[:500])  #Print first 500 characters
+    print(html_content[:100])  #Print first 100 characters
+"""
+
+#Write a function prototyped: def extract(page) to find_all instances of HTML code of repository rows and return it. You should use BeautifulSoup. :-)
+
+def extract(page):
+    soup = BeautifulSoup(page, 'html.parser')
+    all_rows = soup.find_all('article', class_='Box-row')
+    return all_rows
+
+# Testējam abas funkcijas
+url = "https://github.com/trending"
+page_content = request_github_trending(url)
+
+#Testing
+"""
+if page_content:
+    all_rows = extract(page_content)
+    print(f"Found {len(all_rows)} repositories:")
+    for i, repo in enumerate(all_rows[:5], start=1):
+        print(f"\nRepository {i}:\n", repo.prettify())
+"""
+
+#Write a function prototyped: def transform(html_repos) taking an array of all the instances of HTML code of the repository row.
+#It will return an array of hash following this format: [{'developer': NAME, 'repository_name': REPOS_NAME, 'nbr_stars': NBR_STARS}, ...]
+
